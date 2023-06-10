4d78d6498e5ca20c833f137577d9de81522cf62d
Patrik Sandahl <patrik.sandahl@gmail.com>
2023-06-10 23:01:22 +0200

    More Euler stuff.

--- pointwise/euler.py
+++ pointwise/euler.py
@@ -11,7 +11,7 @@ def euler_to_matrix(ypr: ArrayLike, axis_order: str = 'xyz') -> NDArray:
 
     Parameters:
         ypr: Euler angles (yaw, pitch, roll) in radians.
-        axis: Cartesian axis order in which yaw, pitch, roll is applied.
+        axis_order: Cartesian axis order in which yaw, pitch, roll is applied.
 
     Returns:
         Rotation matrix.
@@ -23,22 +23,75 @@ def euler_to_matrix(ypr: ArrayLike, axis_order: str = 'xyz') -> NDArray:
             f"'{axis_order}' is not an implemented axis order")
 
 
-def matrix_to_euler(mat: NDArray, axis_order: str = 'xyz') -> NDArray:
+def matrix_to_euler(R: NDArray, axis_order: str = 'xyz') -> NDArray:
     """
     Decompose a matrix into Euler angles.
 
     Parameters:
         mat: Matrix which upper 3x3 part is a rotation matrix.
-        axis: Cartesian axis order in which yaw, pitch, roll is applied.
+        axis_order: Cartesian axis order in which yaw, pitch, roll is applied.
 
     Returns:
         Array with yaw, pitch and roll in radians.
     """
     if axis_order == 'xyz':
-        return matrix_to_euler_xyz(mat)
+        return matrix_to_euler_xyz(R)
     else:
         raise EulerException(
             f"'{axis_order}' is not an implemented axis order")
+
+
+def homogeneous_matrix_euler(ypr: ArrayLike,
+                             t: ArrayLike,
+                             axis_order: str = 'xyz') -> NDArray:
+    """
+    Create a 4x4 homogeneous matrix from Euler angles and
+    translation.
+
+    Parameters:
+        ypr: Euler angles (yaw, pitch, roll) in radians.
+        t: Translation vector.
+        axis_order: Cartesian axis order in which yaw, pitch, roll is applied.
+
+    Returns:
+        Homogeneous matrix.
+    """
+    return homogeneous_matrix(R=euler_to_matrix(ypr, axis_order=axis_order),
+                              t=t)
+
+
+def homogeneous_matrix(R: NDArray, t: ArrayLike) -> NDArray:
+    """
+    Create a 4x4 homogeneous matrix from R and t.
+
+    Parameters:
+        R: Rotation matrix.
+        t: Translation vector.        
+
+    Returns:
+        Homogeneous matrix.
+    """
+    H = np.hstack((R, np.array(t).reshape(3, 1)))
+    H = np.vstack((H, (0., 0., 0., 1.)))
+
+    return H
+
+
+def H_transform(H: NDArray, X: ArrayLike) -> NDArray:
+    """
+    Transform a coordinate using the honogeneous matrix.
+
+    Parameters:
+        H: Homogeneous matrix.
+        X: 3D coordinate.
+
+    Returns:
+        Transformed 3D coordinate.
+    """
+    Xt = H @ np.append(X, 1.)
+    Xt /= Xt[3]
+
+    return Xt[:3]
 
 
 def euler_xyz_to_matrix(ypr: ArrayLike) -> NDArray:
@@ -51,27 +104,27 @@ def euler_xyz_to_matrix(ypr: ArrayLike) -> NDArray:
     cz = math.cos(z)
     sz = math.sin(z)
 
-    mat = np.empty((3, 3), dtype=np.float64)
+    R = np.empty((3, 3), dtype=np.float64)
 
-    mat[0, 0] = cy * cz
-    mat[0, 1] = -cy * sz
-    mat[0, 2] = sy
+    R[0, 0] = cy * cz
+    R[0, 1] = -cy * sz
+    R[0, 2] = sy
 
-    mat[1, 0] = cz * sx * sy + cx * sz
-    mat[1, 1] = cx * cz - sx * sy * sz
-    mat[1, 2] = -cy * sx
+    R[1, 0] = cz * sx * sy + cx * sz
+    R[1, 1] = cx * cz - sx * sy * sz
+    R[1, 2] = -cy * sx
 
-    mat[2, 0] = -cx * cz * sy + sx * sz
-    mat[2, 1] = cz * sx + cx * sy * sz
-    mat[2, 2] = cx * cy
+    R[2, 0] = -cx * cz * sy + sx * sz
+    R[2, 1] = cz * sx + cx * sy * sz
+    R[2, 2] = cx * cy
 
-    return mat
+    return R
 
 
-def matrix_to_euler_xyz(mat: NDArray) -> NDArray:
-    theta_x = math.atan2(-mat[1, 2], mat[2, 2])
-    theta_y = math.asin(mat[0, 2])
-    theta_z = math.atan2(-mat[0, 1], mat[0, 0])
+def matrix_to_euler_xyz(R: NDArray) -> NDArray:
+    theta_x = math.atan2(-R[1, 2], R[2, 2])
+    theta_y = math.asin(R[0, 2])
+    theta_z = math.atan2(-R[0, 1], R[0, 0])
 
     return np.array((theta_x, theta_y, theta_z))
 
--- pointwise/tests/test_euler.py
+++ pointwise/tests/test_euler.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pointwise.euler import euler_to_matrix, matrix_to_euler
+from pointwise.euler import homogeneous_matrix_euler, H_transform
 
 from numpy.testing import assert_array_almost_equal
 import unittest
@@ -10,12 +11,23 @@ class TestEuler(unittest.TestCase):
     def test_euler_xyz(self: TestEuler) -> None:
         ypr = (0., 0., 0.)
 
-        mat = euler_to_matrix(ypr=ypr, axis_order='xyz')
-        self.assertEqual(mat.shape, (3, 3))
-        assert_array_almost_equal(matrix_to_euler(mat, axis_order='xyz'), ypr)
+        R = euler_to_matrix(ypr=ypr, axis_order='xyz')
+        self.assertEqual(R.shape, (3, 3))
+        assert_array_almost_equal(matrix_to_euler(R, axis_order='xyz'), ypr)
 
         ypr = (1.23, -0.345, -1.56)
 
-        mat = euler_to_matrix(ypr=ypr, axis_order='xyz')
-        self.assertEqual(mat.shape, (3, 3))
-        assert_array_almost_equal(matrix_to_euler(mat, axis_order='xyz'), ypr)
+        R = euler_to_matrix(ypr=ypr, axis_order='xyz')
+        self.assertEqual(R.shape, (3, 3))
+        assert_array_almost_equal(matrix_to_euler(R, axis_order='xyz'), ypr)
+
+    def test_homogeneous_matrix(self: TestEuler) -> None:
+        t = 123., 456., 789.
+
+        H = homogeneous_matrix_euler(ypr=(0., 0., 0.), t=t)
+        self.assertEqual(H.shape, (4, 4))
+
+        X = 0., 0., 0.
+        Xt = H_transform(H, X)
+        self.assertEqual(Xt.shape, (3,))
+        assert_array_almost_equal(Xt, t)
